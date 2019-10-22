2564a7389dec5c964e35d3a6a1d71981501c87da
Puzanira <32363188+Puzanira@users.noreply.github.com>
2019-10-22 23:16:33 +0300

    Update TestRealisations.c

--- TestRealisations.c
+++ TestRealisations.c
@@ -4,11 +4,11 @@
 #include "ParallelRealisation.h"
 #include "SequentualRealisation.h"
 
-void test()
+void test(const char* fileName)
 {
     {
         printf("Test 1 medium file, workers = 3\n");
-        long int resultParallel = process_parallel(FALSE, "TestFiles/test3.txt", 3);
+        long int resultParallel = process_parallel(FALSE, fileName, 3);
         //printf("Result Parallel = %ld", resultParallel);
         long int resultSequentual = process_sequentual("TestFiles/test3.txt");
         //printf("Result Sequentual = %ld", resultSequentual);
@@ -16,9 +16,9 @@ void test()
     }
     {
         printf("Test 2 medium file, workers = 100\n");
-        long int resultParallel = process_parallel(FALSE, "TestFiles/test3.txt", 100);
+        long int resultParallel = process_parallel(FALSE, fileName, 100);
         //printf("Result Parallel = %ld", resultParallel);
-        long int resultSequentual = process_sequentual("TestFiles/test3.txt");
+        long int resultSequentual = process_sequentual(fileName);
         //printf("Result Sequentual = %ld", resultSequentual);
         assert(resultParallel == resultSequentual);
     }
@@ -27,6 +27,9 @@ void test()
 
 int main(int argc, char const *argv[])
 {
-    test();
+    if (argc != 2)
+        return -1;
+    
+    test(argv[1]);
     return 0;
 }

4f3de3a5ad702962fbd6890ae02fdaefb8e2938e
Developer <dev@localhost.localdomain>
2019-10-22 23:49:03 +0300

    Fixed forks

--- TestRealisations.c
+++ TestRealisations.c
@@ -9,20 +9,24 @@ void test(const char* fileName)
     {
         printf("Test 1 medium file, workers = 3\n");
         long int resultParallel = process_parallel(FALSE, fileName, 3);
-        //printf("Result Parallel = %ld", resultParallel);
-        long int resultSequentual = process_sequentual("TestFiles/test3.txt");
-        //printf("Result Sequentual = %ld", resultSequentual);
-        assert(resultParallel == resultSequentual);
+	if (resultParallel) {
+        	//printf("\nResult Parallel = %ld\n", resultParallel);
+        	long int resultSequentual = process_sequentual(fileName);
+        	//printf("\nResult Sequentual = %ld\n", resultSequentual);
+        	assert(resultParallel == resultSequentual);
+
+		{
+        		printf("Test 2 medium file, workers = 100\n");
+        		long int resultParallel = process_parallel(FALSE, fileName, 100);
+        		if (resultParallel) {
+        			//printf("\nResult Parallel = %ld\n", resultParallel);
+        			long int resultSequentual = process_sequentual(fileName);
+        			//printf("\nResult Sequentual = %ld\n", resultSequentual);
+        			assert(resultParallel == resultSequentual);
+			}
+    		}
+	}
     }
-    {
-        printf("Test 2 medium file, workers = 100\n");
-        long int resultParallel = process_parallel(FALSE, fileName, 100);
-        //printf("Result Parallel = %ld", resultParallel);
-        long int resultSequentual = process_sequentual(fileName);
-        //printf("Result Sequentual = %ld", resultSequentual);
-        assert(resultParallel == resultSequentual);
-    }
-    printf("passed");
 }
 
 int main(int argc, const char *argv[])

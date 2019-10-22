ee911d15dc8e3079041853286339c1289f0a9c09
Puzanira <puzanova.ira2011@yandex.ru>
2019-10-22 21:50:47 +0300

    Added CMake and Tests

---
+++ Tests/TestRealisations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "ParallelRealisation.h"
+#include "SequentualRealisation.h"
+
+void test()
+{
+    {
+        printf("Test 1 medium file, workers = 3\n");
+        long int resultParallel = process_parallel(FALSE, "TestFiles/test3.txt", 3);
+        //printf("Result Parallel = %ld", resultParallel);
+        long int resultSequentual = process_sequentual("TestFiles/test3.txt");
+        //printf("Result Sequentual = %ld", resultSequentual);
+        assert(resultParallel == resultSequentual);
+    }
+    {
+        printf("Test 2 medium file, workers = 100\n");
+        long int resultParallel = process_parallel(FALSE, "TestFiles/test3.txt", 100);
+        //printf("Result Parallel = %ld", resultParallel);
+        long int resultSequentual = process_sequentual("TestFiles/test3.txt");
+        //printf("Result Sequentual = %ld", resultSequentual);
+        assert(resultParallel == resultSequentual);
+    }
+    printf("passed");
+}
+
+int main(int argc, char const *argv[])
+{
+    test();
+    return 0;
+}
--- main.c
+++ main.c
@@ -3,12 +3,26 @@
 #include "ParallelRealisation.h"
 #include "SequentualRealisation.h"
 
+/*  Передаются параметры: 
+    1: 0, если debug == FALSE, иначе 1
+    2: имя файла для работы с ним
+    3: количество процессов-детей
+*/
 int main(int argc, char const *argv[])
 {
-    long int resultParallel = process_parallel(FALSE, "TestFiles/test3.txt", 3);
+    if (argc != 4)
+    {
+        return -1;
+    }
+
+    int debug = argv[1];
+    char* fileName = argv[2];
+    int workers = argv[3];
+
+    long int resultParallel = process_parallel(debug, fileName, 3);
     printf("Result Parallel = %ld", resultParallel);
 
-    long int resultSequentual = process_sequentual("TestFiles/test3.txt");
+    long int resultSequentual = process_sequentual(fileName);
     printf("Result Sequentual = %ld", resultSequentual);
     return 0;
 }
